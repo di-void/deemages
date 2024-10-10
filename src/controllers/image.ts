011ede83dfb6a975a4912791f936a7d83f34d52c
DEE <donzydonathan@gmail.com>
2024-10-10 17:25:20 +0100

    chore: cleanup

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -3,7 +3,6 @@ import { UserType } from "../utils/validators";
 import { formatRegularErrorMessage } from "../utils/helpers";
 import sharp from "sharp";
 import fs from "node:fs";
-import { FILE_STORAGE_LOCATION } from "../config";
 
 const HUNDRED_KB = 100 * 1024;
 
@@ -16,18 +15,14 @@ export async function uploadImage(req: Request, res: Response) {
 
     // check file size
     if (req.file!.size >= HUNDRED_KB) {
-      const newFilePath =
-        `${FILE_STORAGE_LOCATION}/compressed-` + req.file?.filename;
-
       const inputBuffer = fs.readFileSync(filePath);
 
-      // compress or scale down images bigger than 100kb
+      // scale down images bigger than 100kb
       const outputBuffer = await sharp(inputBuffer)
         .resize({ width: 800 })
         .toBuffer();
 
       fs.writeFileSync(filePath, outputBuffer);
-      // fs.unlinkSync(filePath);
     }
 
     // get storage path
@@ -39,6 +34,7 @@ export async function uploadImage(req: Request, res: Response) {
     });
   } catch (error) {
     console.error(error);
+
     return res.status(500).json({
       message: "error",
       error: formatRegularErrorMessage("something went wrong"),
