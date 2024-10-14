41315183b1016fd1272078c94818564dd3534997
DEE <donzydonathan@gmail.com>
2024-10-14 19:00:53 +0100

    chore: extract zod error helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,6 +2,7 @@ import { Request } from "express";
 import multer from "multer";
 import { Mime } from "./validators";
 import { ENVIRONMENT, PORT, API_VERSION } from "../config";
+import { ZodError } from "zod";
 
 export function formatRegularErrorMessage(errorMsg: string) {
   return [{ message: errorMsg }];
@@ -25,4 +26,8 @@ export function generatePublicURL(fileName: string, req: Request) {
   const proto = req.secure ? "https" : "http";
   const port = ENVIRONMENT === "development" ? `:${PORT}` : "";
   return `${proto}://${req.hostname}${port}/api/${API_VERSION}/${fileName}`;
+}
+
+export function formatZodError<T>(error: ZodError<T>) {
+  return error.issues.map(({ message, path }) => ({ message, path }));
 }
