aaecc883b70e10e4e4b9c12f55fa758df829c54b
DEE <donzydonathan@gmail.com>
2024-10-23 12:48:43 +0100

    refactor: place transform functions in separate file

---
+++ src/controllers/image/transformers.ts
@@ -0,0 +1,44 @@
+import { Buffer } from "node:buffer";
+import sharp from "sharp";
+import {
+  type CropType,
+  type ResizeType,
+  type FormatType,
+} from "../../utils/validators";
+
+export async function resizeImage(
+  data: Buffer,
+  params: ResizeType
+): Promise<Buffer> {
+  // resize image
+  const resized = await sharp(data)
+    .resize({ width: params.width, height: params.height })
+    .toBuffer();
+  return resized;
+}
+
+// todo: is cropping validation necessary?
+export async function cropImage(
+  data: Buffer,
+  params: CropType
+): Promise<Buffer> {
+  // crop image
+  const cropped = await sharp(data)
+    .extract({
+      left: params.x,
+      top: params.y,
+      width: params.width,
+      height: params.height,
+    })
+    .toBuffer();
+  return cropped;
+}
+
+export async function changeImageFormat(
+  data: Buffer,
+  params: FormatType
+): Promise<Buffer> {
+  // change image format
+  const formatted = await sharp(data).toFormat(params).toBuffer();
+  return formatted;
+}
